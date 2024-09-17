f2006349cd1eff0359abec0671ed7a9d27478680
Ivan Tandella <ivantandella2@gmail.com>
2024-09-17 11:26:20 +0700

    fix: invalid date bug in mobile

--- utils/date.ts
+++ utils/date.ts
@@ -6,7 +6,8 @@ export function convertDate(timestamp: string) {
 export function timeAgo(timestamp: string) {
   const moment = require("moment");
 
-  const date = moment(convertDate(timestamp), "M/D/YYYY, h:mm:ss A");
+  // const date = moment(convertDate(timestamp), "M/D/YYYY, h:mm:ss A");
+  const date = moment(timestamp);
 
   const relativeTime = date.fromNow();
   return relativeTime;
